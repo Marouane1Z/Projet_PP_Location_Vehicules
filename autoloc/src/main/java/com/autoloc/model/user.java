fbedd71ca837ff14d008174bf7f3d0a3aa58ff94
Rayane <harkati.rayane.2>
2026-04-16 16:04:06 +0200

    Ajout user

--- autoloc/src/main/java/com/autoloc/model/user.java
+++ autoloc/src/main/java/com/autoloc/model/user.java
@@ -1,92 +1,53 @@
 package com.autoloc.model;
-
 import com.autoloc.enums.*;
+import jakarta.persistence.*;
+import lombok.*;
 
-public abstact class user {
-    Long id ;
+
+//@Entity              → "je suis une table"
+//@Table               → "mon nom en base est..."
+//@Inheritance         → "mes enfants ont leur propre table"
+//@Getter / @Setter    → "génère mes getters/setters"
+//@NoArgsConstructor   → "génère User() vide"
+//@AllArgsConstructor  → "génère User(tous les champs)"
+
+
+@Entity
+@Table(name = "utilisateur")
+@Inheritance(strategy = InheritanceType.JOINED)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class user {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+
+    @Column(nullable = false)
     String firstname;
+    @Column(nullable = false)
     String lastname;
+
+    @Column(nullable = false)
     int age;
+
+    @Column(nullable = false, unique = true)
     String email;
+
+    @Column(nullable = false)
+    private String password;
+
     String phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     userRole role ;
+
     String address;
 
-    public Long getId() {
-        return id;
-    }
+    private Boolean actif = true;
 
-    public String getFirstname() {
-        return firstname;
-    }
 
-    public String getLastname() {
-        return lastname;
-    }
 
-    public int getAge() {
-        return age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public userRole getRole() {
-        return role;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public void setRole(userRole role) {
-        this.role = role;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    @Override
-    public String toString() {
-        return "user{" +
-                "id=" + id +
-                ", firstname='" + firstname + '\'' +
-                ", lastname='" + lastname + '\'' +
-                ", age=" + age +
-                ", email='" + email + '\'' +
-                ", phone='" + phone + '\'' +
-                ", role=" + role +
-                ", address='" + address + '\'' +
-                '}';
-    }
 }
